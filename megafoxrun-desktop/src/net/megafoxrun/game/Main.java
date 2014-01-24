e5c09135d9a8187cb31896f301e78804988d2650
Janne Vikstedt <janne.vikstedt@metropolia.fi>
2014-01-24 10:15:30 +0200

    Test change

--- megafoxrun-desktop/src/net/megafoxrun/game/Main.java
+++ megafoxrun-desktop/src/net/megafoxrun/game/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "megafoxrun";
+		cfg.title = "Mega Fox Run";
 		cfg.useGL20 = false;
 		cfg.width = 480;
 		cfg.height = 320;
