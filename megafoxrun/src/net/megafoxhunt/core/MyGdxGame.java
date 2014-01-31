0d237c0e54e1fe11d239f598cf36c3c5f4460986
Janne Vikstedt <janne.vikstedt@metropolia.fi>
2014-01-31 11:56:53 +0200

    android client-server test

--- megafoxrun/src/net/megafoxhunt/core/MyGdxGame.java
+++ megafoxrun/src/net/megafoxhunt/core/MyGdxGame.java
@@ -10,6 +10,7 @@ import net.megafoxhunt.server.KryoNetwork.Login;
 
 import com.esotericsoftware.kryonet.Listener.ThreadedListener;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -26,31 +27,38 @@ public class MyGdxGame extends Game {
 		client.start();
 		
 		KryoNetwork.register(client);
-		
+		System.out.println("test");
 		client.addListener(new ThreadedListener(new Listener() {
+			@Override
 			public void connected (Connection connection) {
+				System.out.println("connected");
 			}
 
+			@Override
 			public void received (Connection connection, Object object) {
+				System.out.println(object);
+				System.out.println(connection.getID());
 				if (object instanceof WelcomePlayer) {
 					WelcomePlayer wp = (WelcomePlayer)object;
 					System.out.println("id: " + wp.id);
 				}
 			}
 
+			@Override
 			public void disconnected (Connection connection) {
 				System.exit(0);
 			}
 		}));
+		
 		
 		try {
 			client.connect(5000, "10.112.123.242", 6666);
 			Login login = new Login();
 			login.name = "Testi nimi";
 			client.sendTCP(login);
-			} catch (IOException ex) {
+			System.out.println("sending");
+		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
-
 }
