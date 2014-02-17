84f1ee1332d8804b1253e93dd7fb08499ee7e923
Janne Vikstedt <vikstedt.janne@gmail.com>
2014-02-17 19:31:01 +0200

    test

--- megafoxrun/src/net/megafoxhunt/core/MyGdxGame.java
+++ megafoxrun/src/net/megafoxhunt/core/MyGdxGame.java
@@ -19,7 +19,7 @@ public class MyGdxGame extends Game {
 			
 		GameNetwork.init();
 		GameNetwork.setUsername("TestUser");		
-		GameNetwork.connect("localhost", 6666);
+		GameNetwork.connect("192.168.0.102", 6666);
 		
 		INSTANCE.setScreen(new MenuScreen());	
 	}	
