e7934dd7f6fd71fdbae940ad58461979a7cc3b89
Miroko <MK>
2014-04-11 22:17:38 +0300

    set player name

--- megafoxrun/src/net/megafoxhunt/ui/MenuUI.java
+++ megafoxrun/src/net/megafoxhunt/ui/MenuUI.java
@@ -1,13 +1,22 @@
 package net.megafoxhunt.ui;
 
+import java.text.Normalizer.Form;
+
 import net.megafoxhunt.core.MyGdxGame;
 
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle;
 
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
@@ -17,10 +26,24 @@ public class MenuUI extends Table{
 	
 	private ImageButton connectButton;
 	private ImageButton quitButton;
+	private TextField nameField;
 	
 	public MenuUI(){
 		setFillParent(true);
-
+				
+		TextFieldStyle fieldStyle = new TextFieldStyle();
+		fieldStyle.font = new BitmapFont();
+		fieldStyle.fontColor = Color.BLACK;
+		
+		Pixmap background = new Pixmap(120,30, Format.RGB888);
+		background.setColor(Color.WHITE);
+		background.fill();
+		
+		TextureRegionDrawable backgroundImage = new TextureRegionDrawable(new TextureRegion(new Texture(background)));
+		
+		fieldStyle.background = backgroundImage;
+		nameField = new TextField("player", fieldStyle);
+		
 		TextureRegionDrawable connectButtonImage = new TextureRegionDrawable(new TextureRegion(MyGdxGame.resources.connectButtonTexture));
 		
 		connectButton = new ImageButton(connectButtonImage);	
@@ -28,7 +51,7 @@ public class MenuUI extends Table{
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {		
 				if(connectButton.isPressed()){					
-					MyGdxGame.network.setUsername("TestUser");	
+					MyGdxGame.network.setUsername(nameField.getText());	
 					MyGdxGame.network.start();	
 					MyGdxGame.network.connect(MyGdxGame.IP_SERVER, 6666);
 				}
@@ -47,7 +70,9 @@ public class MenuUI extends Table{
 			}
 		});				
 		
-		add(connectButton);
+		add(nameField);
+		row();
+		add(connectButton).padTop(20);
 		row();
 		add(quitButton).padTop(20);
 
